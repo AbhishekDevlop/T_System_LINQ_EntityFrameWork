db08c6e78bc1dee5f6f12ff9ef87f09d48bd76d2
AbhishekDevlop <101594544+AbhishekDevlop@users.noreply.github.com>
2022-03-29 18:47:56 +0530

    Add files via upload
    Entity framework

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace LINQ_To_Entity_Fw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.labSalary = new System.Windows.Forms.Label();
+            this.txtDesignation = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.labDesignation = new System.Windows.Forms.Label();
+            this.labName = new System.Windows.Forms.Label();
+            this.labId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(377, 406);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 38);
+            this.btnUpdate.TabIndex = 36;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(508, 406);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 38);
+            this.btnSave.TabIndex = 35;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(740, 172);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 36);
+            this.btnDelete.TabIndex = 34;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(740, 116);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 34);
+            this.btnSearch.TabIndex = 33;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(377, 324);
+            this.txtSalary.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(232, 22);
+            this.txtSalary.TabIndex = 32;
+            // 
+            // labSalary
+            // 
+            this.labSalary.AutoSize = true;
+            this.labSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labSalary.Location = new System.Drawing.Point(225, 326);
+            this.labSalary.Name = "labSalary";
+            this.labSalary.Size = new System.Drawing.Size(52, 16);
+            this.labSalary.TabIndex = 31;
+            this.labSalary.Text = "Salary";
+            // 
+            // txtDesignation
+            // 
+            this.txtDesignation.Location = new System.Drawing.Point(377, 252);
+            this.txtDesignation.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDesignation.Name = "txtDesignation";
+            this.txtDesignation.Size = new System.Drawing.Size(232, 22);
+            this.txtDesignation.TabIndex = 30;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(377, 177);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(232, 22);
+            this.txtName.TabIndex = 29;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(377, 110);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(232, 22);
+            this.txtId.TabIndex = 28;
+            // 
+            // labDesignation
+            // 
+            this.labDesignation.AutoSize = true;
+            this.labDesignation.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labDesignation.Location = new System.Drawing.Point(225, 252);
+            this.labDesignation.Name = "labDesignation";
+            this.labDesignation.Size = new System.Drawing.Size(90, 16);
+            this.labDesignation.TabIndex = 27;
+            this.labDesignation.Text = "Designation";
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labName.Location = new System.Drawing.Point(225, 180);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(48, 16);
+            this.labName.TabIndex = 26;
+            this.labName.Text = "Name";
+            // 
+            // labId
+            // 
+            this.labId.AutoSize = true;
+            this.labId.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labId.Location = new System.Drawing.Point(225, 116);
+            this.labId.Name = "labId";
+            this.labId.Size = new System.Drawing.Size(20, 16);
+            this.labId.TabIndex = 25;
+            this.labId.Text = "Id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.labSalary);
+            this.Controls.Add(this.txtDesignation);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.labDesignation);
+            this.Controls.Add(this.labName);
+            this.Controls.Add(this.labId);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "EntityFrameWorkDemo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label labSalary;
+        private System.Windows.Forms.TextBox txtDesignation;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label labDesignation;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.Label labId;
+    }
+}
+
---
+++ LINQ_To_Entity_Fw/LINQ_To_Entity_Fw/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LINQ_To_Entity_Fw
+{
+    public partial class Form1 : Form
+    {
+        EmployeeEntity dbContext =  new EmployeeEntity();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Clear() 
+        {
+            txtId.Clear();
+            txtDesignation.Clear();
+            txtName.Clear();
+            txtSalary.Clear();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.Id = Convert.ToInt32(txtId.Text);
+            emp.Name = txtName.Text;
+            emp.Designation = txtDesignation.Text;
+            emp.Salary = Convert.ToDecimal(txtSalary.Text);
+
+            dbContext.Employees.Add(emp);
+           int result = dbContext.SaveChanges(); //  reflect changes to the database
+            if(result == 1) 
+            {
+                MessageBox.Show("Record Inserted");
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee emp = dbContext.Employees.Find(Convert.ToInt32(txtId.Text));
+                if (emp != null)
+                {
+                    emp.Name = txtName.Text;
+                    emp.Designation = txtDesignation.Text;
+                    emp.Salary = Convert.ToDecimal(txtSalary.Text);
+                    int result = dbContext.SaveChanges();
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record is updated");
+                        Clear();
+                    }
+                }
+                else { MessageBox.Show("Record not found"); Clear(); }
+             }catch(Exception ex) { MessageBox.Show(ex.Message);}
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee emp = dbContext.Employees.Find(Convert.ToInt32(txtId.Text));
+
+                if (emp != null)
+                {
+                    txtName.Text = emp.Name;
+                    txtDesignation.Text = emp.Designation;  
+                    txtSalary.Text = emp.Salary.ToString();
+                }
+                else { MessageBox.Show("Record not found");Clear(); }
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message);}
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee emp = dbContext.Employees.Find(Convert.ToInt32(txtId.Text));
+
+                if (emp != null)
+                {
+                    dbContext.Employees.Remove(emp);
+                    int result = dbContext.SaveChanges();//reflect changes to database
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted"); Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found"); Clear();
+                }
+            }catch(Exception ex) { MessageBox.Show(ex.Message);}
+        }
+    }
+}
